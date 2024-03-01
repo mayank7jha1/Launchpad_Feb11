49649db4b681c1ed70e6b89354cc2b50d62e5010
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2024-03-01 22:08:23 +0530

    Lecture 1 & 3

---
+++ Lecture 3/Start.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using  namespace std;
+
+int main() {
+	int mayank;//Variable Declaration
+	mayank = 10;//Variable Initialisation
+
+	//int mayank = 100;//Reclaration is not allowed.
+	mayank = 100;//Reinitialisation
+	int samyak = 100;//Variable Declaration and Initialisation
+
+	int x;
+	/*
+
+		My name is Mayank.
+		I am a Boy.
+
+	*/
+	//My name is Mayank.
+	//I am a Boy.
+
+	cin >> x;//>> Extractor Operator
+	cout << x;//<< Insertion Operator
+}
+
+
+
+
+
+
+
+
---
+++ Lecture 3/Start2.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using  namespace std;
+
+int main() {
+	// int x;
+	// int y;
+	// // cin >> x;
+	// // cin >> y;
+	// // int xy;
+	// cin >> x >> y;
+	// cout << x << " " << y;
+
+	// cout << " Mayank";
+
+	//New Line:
+
+	cout << "Mayank" << '\n';
+	cout << "Samyak" << '\n';
+	cout << "Check";
+}
