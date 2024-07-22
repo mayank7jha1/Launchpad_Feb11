9ef76c5f9b1d8f3a6f879d5e6f2fe09bba02ffbd
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2024-07-22 20:13:57 +0530

    Update Linked_List.h

--- Lecture 36/Linked_List.h
+++ Lecture 36/Linked_List.h
@@ -141,7 +141,138 @@ node<int>*MidPoint3(node<int>*head) {
 	return slow;
 }
 
+//Reverse a Linked List:
 
+node<int>*Reverse(node<int>*head) {
+
+	node<int>* current = head;
+	node<int>*prev = NULL;
+	node<int>*n;
+
+	while (current != NULL) {
+		n = current->next;
+		current->next = prev;
+		prev = current;
+		current = n;
+	}
+	// head = prev;
+	return prev;
+}
+
+
+node<int>*ReverseRecursively(node<int>*head) {
+
+	if (head->next == NULL or head == NULL) {
+		return head;
+	}
+
+
+	node<int>*head2 = ReverseRecursively(head->next);
+
+	node<int>*current = head;
+	current->next->next = current;
+	current->next = NULL;
+
+	return head2;
+}
+
+
+void CycleCreation(node<int>*head) {
+
+	int count = 4;
+
+	node<int>*temp = head;
+
+	//temp ko last node par lejao.
+
+	while (temp->next != NULL) {
+		temp = temp->next;
+	}
+
+	//cout << temp->data << endl;
+
+	//temp is now my last node.
+
+	//head ko mujhe 4 par laana hain.
+
+	while (count > 1) {
+		head = head->next;
+		count = count - 1;
+	}
+
+	//cout << head->data << endl;
+
+	temp->next = head;
+}
+
+
+bool isCycle(node<int>*head) {
+
+	node<int>*slow = head;
+	node<int>*fast = head;
+
+
+	while (fast != NULL and fast->next != NULL) {
+
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow) {
+			//cout << "Yes There is a Cycle" << endl;
+			return true;
+		}
+	}
+
+	return false;
+}
+
+
+void CycleRemoval(node<int>*head) {
+
+	if (isCycle(head) == 0) {
+		return;
+	}
+	node<int>*slow = head;
+	node<int>*fast = head;
+
+
+	while (fast != NULL and fast->next != NULL) {
+
+		fast = fast->next->next;
+		slow = slow->next;
+
+		if (fast == slow) {
+			break;
+		}
+	}
+
+	//You have your Meeting Point.
+	slow = head;
+	node<int>*prev = head;
+
+	while (prev->next != fast) {
+		prev = prev->next;
+	}
+
+	/*
+		slow is pointing to head.
+		fast is pointing to Meeting Point.
+
+		prev is pointing to fast se ek kadam pehle.
+	*/
+
+	while (fast != slow) {
+		fast = fast->next;
+		slow = slow->next;
+		prev = prev->next;
+	}
+
+	//Slow and Fast are currently at the origin of the
+	//cycle.
+	//prev is at the last node.
+
+	prev->next = NULL;
+}
 
 
 
