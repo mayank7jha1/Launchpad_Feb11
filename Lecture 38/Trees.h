cb0e968a9973fc79ecc000bf5d2e2958018243a5
Mayank Jha <70832880+mayank7jha1@users.noreply.github.com>
2024-07-27 23:48:16 +0530

    Update Trees.h

--- Lecture 38/Trees.h
+++ Lecture 38/Trees.h
@@ -1,4 +1,6 @@
 #include<iostream>
+#include<climits>
+#include<algorithm>
 using  namespace std;
 
 template<typename T>
@@ -166,27 +168,149 @@ void Mirror(node<int>*root) {
 }
 
 
-//Diameter of Tree:
-int maxi = INT_MIN;
 
+//Diameter of a Tree:
 int Diameter(node<int>*root) {
 
 	if (root == NULL) {
 		return 0;
 	}
 
+	int Op1 = Diameter(root->left);
+	int Op2 = Diameter(root->right);
+	int Op3 = Height(root->left) + Height(root->right);
 
-	int LSH = Diameter(root->left);
-	int RSH = Diameter(root->right);
+	return max({Op1, Op2, Op3});
+}
 
-	maxi = max(maxi, LSH + RSH + 1);
+
+//Optimised Diameter: n Computation
+class Pair {
+public:
+	int Height;
+	int Diameter;
+
+	Pair() {
+		Height = 0;
+		Diameter = 0;
+	}
+};
+
+
+Pair Diameter2(node<int>*root) {
+
+	Pair x;
+
+	if (root == NULL) {
+		x.Height = 0;
+		x.Diameter = 0;
+		return  x;
+	}
+
+
+	Pair Left_Subtree = Diameter2(root->left);
+	Pair Right_Subtree = Diameter2(root->right);
+
+	//I Have To do 2 things: Height calculate
+	//karni hain and diametere calculate karna hain.
+
+	x.Height = max(Left_Subtree.Height,
+	               Right_Subtree.Height) + 1;
+
+	x.Diameter = max({Left_Subtree.Diameter,
+	                  Right_Subtree.Diameter,
+	                  Left_Subtree.Height + Right_Subtree.Height
+	                 });
+
+	return x;
 }
 
 
 
+//Diameter of Tree 3:
+int maxi = 0;
+
+int Diameter3(node<int>*root) {
+
+	if (root == NULL) {
+		return 0;
+	}
 
 
+	int LSH = Diameter3(root->left);
+	int RSH = Diameter3(root->right);
 
+	maxi = max(maxi, LSH + RSH);
+
+	int Height = 1 + max(LSH, RSH);
+	return Height;
+}
+
+
+//Inbuilt Pair:
+/*
+	pair<int,int>p1:
+	p1.first: balanced hain ki nahi.
+	p1.second: Height kya hain.
+
+*/
+
+pair<bool, int>isHeightBalanced(node<int>*root) {
+
+	pair<bool, int>x;
+
+	if (root == NULL) {
+		x.first = 1;
+		x.second = 0;
+		return x;
+	}
+
+	pair<bool, int>LS = isHeightBalanced(root->left);
+	pair<bool, int>RS = isHeightBalanced(root->right);
+	x.second = max(LS.second, RS.second) + 1;
+
+	if (LS.first == 1 and RS.first == 1 and
+	        abs(LS.second - RS.second) <= 1) {
+		x.first = 1;
+	} else {
+		x.first = 0;
+	}
+
+	return x;
+
+}
+
+//Print the kth Level: Indexing is 1 based.
+
+void PrintkthLevel(node<int>*root, int k) {
+
+	if (root == NULL) {
+		return;
+	}
+
+	if (k == 1) {
+		cout << root->data << " ";
+		return;
+	}
+
+	PrintkthLevel(root->left, k - 1);
+	PrintkthLevel(root->right, k - 1);
+
+	return;
+}
+
+//n^2:
+void PrintALLLevel(node<int>*root) {
+
+	int H = Height(root);
+
+	for (int i = 1; i <= H; i = i + 1) {
+
+		PrintkthLevel(root, i);
+		cout << endl;
+	}
+
+}
 
 
 
